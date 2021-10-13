74323a5630a451b3d6e0a853c281a31dd327c92f
Benjamin Wollenweber <ich@wollis-page.de>
2021-10-13 20:40:21 +0200

    ADD init commit

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'align_multiline_comment' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'blank_line_before_statement' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'combine_consecutive_issets' => true,
+        'combine_consecutive_unsets' => true,
+        'compact_nullable_typehint' => true,
+        'heredoc_to_nowdoc' => true,
+        'is_null' => true,
+        'list_syntax' => ['syntax' => 'long'],
+        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+        'no_null_property_initialization' => true,
+        'echo_tag_syntax' => true,
+        'no_superfluous_elseif' => true,
+        'no_unneeded_curly_braces' => true,
+        'no_unneeded_final_method' => true,
+        'no_unreachable_default_argument_value' => false,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'ordered_class_elements' => false,
+        'ordered_imports' => true,
+        'phpdoc_to_comment' => false,
+        'php_unit_strict' => false,
+        'php_unit_test_class_requires_covers' => false,
+        'phpdoc_add_missing_param_annotation' => true,
+        'phpdoc_no_package' => false,
+        'phpdoc_order' => true,
+        'protected_to_private' => false,
+        'semicolon_after_instruction' => true,
+        'single_line_comment_style' => true,
+        'strict_comparison' => false,
+        'strict_param' => false,
+        'yoda_style' => false,
+        'class_attributes_separation' => true,
+        'declare_strict_types' => true,
+        'blank_line_after_opening_tag' => false,
+        'no_extra_blank_lines' => false,
+        'single_line_throw' => false,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in([
+                __DIR__ . DIRECTORY_SEPARATOR . 'src',
+                __DIR__ . DIRECTORY_SEPARATOR . 'tests',
+            ])
+            ->name('*.php')
+    )
+;
