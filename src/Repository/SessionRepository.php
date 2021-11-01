9e4f714b848823c4223fda85ecd77170fd5e917a
Benjamin Wollenweber <ich@wollis-page.de>
2021-11-01 11:02:01 +0100

    Refactor repositories

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -3,25 +3,21 @@ declare(strict_types=1);
 
 namespace GibsonOS\Module\Transfer\Repository;
 
-use GibsonOS\Core\Exception\DateTimeError;
 use GibsonOS\Core\Exception\Repository\SelectError;
+use GibsonOS\Core\Model\AbstractModel;
 use GibsonOS\Core\Repository\AbstractRepository;
 use GibsonOS\Module\Transfer\Model\Session;
 
+/**
+ * @method Session fetchOne(string $where, array $parameters, string $abstractModelClassName = AbstractModel::class)
+ */
 class SessionRepository extends AbstractRepository
 {
     /**
-     * @throws DateTimeError
      * @throws SelectError
      */
     public function getById(int $id): Session
     {
-        $model = $this->fetchOne('`id`=?', [$id], Session::class);
-
-        if (!$model instanceof Session) {
-            throw new SelectError();
-        }
-
-        return $model;
+        return $this->fetchOne('`id`=?', [$id], Session::class);
     }
 }
